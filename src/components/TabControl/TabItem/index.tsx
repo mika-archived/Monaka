bbe94da10c7a35626e82a2e7a5988c5ed0c059f9
Mikazuki <mikazuki_fuyuno@outlook.com>
2020-03-26 01:31:08 +0900

    fix: use `stopPropagation`

--- src/components/TabControl/TabItem/index.tsx
+++ src/components/TabControl/TabItem/index.tsx
@@ -7,6 +7,7 @@ import { IconContext } from "@/components/IconProvider";
 import { ThemeContext, Theme } from "@/components/ThemeProvider";
 
 type Props = {
+  className?: string;
   item: TabContent;
   isActivated: boolean;
   onCloseButtonClicked?: (item: TabContent) => void;
@@ -22,9 +23,11 @@ const TabWrapper = styled.div<TabWrapperProps>`
   display: inline-block;
   min-width: 150px;
   padding: 8px 16px;
+  overflow: hidden;
   color: ${(props) => (props.isActivated ? props.theme.activeFontColor : props.theme.fontColor)};
+  text-overflow: ellipsis;
   user-select: none;
-  background-color: ${(props) => (props.isActivated ? props.theme.activeBackground : props.theme.background)};
+  background-color: ${(props) => (props.isActivated ? props.theme.activeBackground : props.theme.inactiveBackground)};
 `;
 
 const TabContainer = styled.div`
@@ -40,13 +43,18 @@ const Label = styled.span`
   flex: 1 1 auto;
   padding-left: 8px;
   font-size: 14px;
+  white-space: nowrap;
 `;
 
 const CloseButton = styled(Close)`
-  padding: 2px;
+  padding: 2px 4px;
 `;
 
-const TabItem: React.FC<Props> = ({ item, isActivated, onCloseButtonClicked, onTabClicked }) => {
+const EmptySpace = styled.div`
+  width: 24px;
+`;
+
+const TabItem: React.FC<Props> = ({ className, item, isActivated, onCloseButtonClicked, onTabClicked }) => {
   const [isHovered, setHovered] = useState(false);
 
   const getIconComponent = (icons: { extension: RegExp; component: React.FC<any> }[], filename: string) => {
@@ -60,20 +68,29 @@ const TabItem: React.FC<Props> = ({ item, isActivated, onCloseButtonClicked, onT
     if (onTabClicked) onTabClicked(item);
   };
 
-  const onClickCloseButton = () => {
+  const onClickCloseButton = (event: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
+    event.stopPropagation();
+
     if (onCloseButtonClicked) onCloseButtonClicked(item);
   };
 
   return (
     <ThemeContext.Consumer>
       {(theme) => (
-        <TabWrapper isActivated={isActivated} theme={theme} onClick={onClickTabWrapper} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
+        <TabWrapper
+          className={className}
+          isActivated={isActivated}
+          theme={theme}
+          onClick={onClickTabWrapper}
+          onMouseEnter={() => setHovered(true)}
+          onMouseLeave={() => setHovered(false)}
+        >
           <IconContext.Consumer>
             {(icons) => (
               <TabContainer>
                 <Icon>{getIconComponent(icons, item.item.title)}</Icon>
                 <Label>{item.item.title}</Label>
-                {isActivated || isHovered ? <CloseButton onClick={onClickCloseButton} /> : null}
+                {isActivated || isHovered ? <CloseButton onClick={onClickCloseButton} /> : <EmptySpace />}
               </TabContainer>
             )}
           </IconContext.Consumer>
