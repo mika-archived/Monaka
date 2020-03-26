0cd9bc94fb9b36ca9a317e5d9fd6a1104815c8f3
Mikazuki <mikazuki_fuyuno@outlook.com>
2020-03-27 01:19:14 +0900

    feat: add explorer item component

---
+++ src/components/Explorer/ExplorerItem/index.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import AnimateHeight from "react-animate-height";
+import styled from "styled-components";
+
+import { ExplorerSectionItem } from "@/types";
+import { ChevronDown, ChevronRight } from "@/components/Icon";
+import Scroller from "@/components/Scroller";
+import { ThemeContext, Theme } from "@/components/ThemeProvider";
+
+type Props = {
+  className?: string;
+} & ExplorerSectionItem;
+
+type ThemedProps = {
+  theme: Theme;
+};
+
+const Content = styled.div`
+  padding: 2px;
+`;
+
+const Icon = styled.div`
+  padding: 2px 0;
+`;
+
+const Label = styled.div<ThemedProps>`
+  flex: 1 1 auto;
+  padding: 0 0 2px 2px;
+  overflow: hidden;
+  font-size: 14px;
+  color: ${(props) => props.theme.fontColor};
+  text-overflow: ellipsis;
+`;
+
+const Title = styled.div<ThemedProps>`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 2px 0 0 0;
+  user-select: none;
+  background: ${(props) => props.theme.inactiveBackground};
+`;
+
+const Wrapper = styled.div<ThemedProps>`
+  overflow: hidden;
+`;
+
+const ExplorerItem: React.FC<Props> = ({ children, className, title }) => {
+  const [isExpanded, setExpanded] = useState(false);
+
+  const toggleExpanded = () => setExpanded(!isExpanded);
+
+  return (
+    <ThemeContext.Consumer>
+      {(theme) => (
+        <Wrapper className={className} theme={theme}>
+          <Title onClick={toggleExpanded} theme={theme}>
+            <Icon>{isExpanded ? <ChevronDown /> : <ChevronRight />}</Icon>
+            <Label theme={theme}>{title}</Label>
+          </Title>
+          <AnimateHeight duration={100} height={isExpanded ? "auto" : 0}>
+            {/* expand animation not working */}
+            <Scroller>
+              <Content>{children}</Content>
+            </Scroller>
+          </AnimateHeight>
+        </Wrapper>
+      )}
+    </ThemeContext.Consumer>
+  );
+};
+
+export default ExplorerItem;
