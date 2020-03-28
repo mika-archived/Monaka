9c7af3553dd1dc13184a1378a01569f79d1d6645
Mikazuki <mikazuki_fuyuno@outlook.com>
2020-03-28 19:25:40 +0900

    fix: monaco-editor requires MonacoEnvironment global conf

--- src/components/MonacoEditor/index.tsx
+++ src/components/MonacoEditor/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
 import { ControlledEditor } from "@monaco-editor/react";
 
@@ -16,6 +16,42 @@ type Props = {
 
 const Editor: React.FC<Props> = ({ className, language, onContentChanged, onEditorMounted, options, value }) => {
   const noop = () => {};
+
+  useEffect(() => {
+    if (!(window as any).MonacoEnvironment) {
+      (window as any).MonacoEnvironment = {
+        getWorkerUrl(moduleId: string, label: string): string {
+          // Workaround: Monaco Editor could not create a worker when loaded from CDN.
+          const wrapForCrossDomain = (url: string): string => {
+            return `data:text/javascript;charset=utt-8,${encodeURIComponent(`
+              self.MonacoEnvironment = {
+                baseUrl: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.19.3'
+              };
+              importScripts('https://cdn.jsdelivr.net/npm/monaco-editor@0.19.3${url}');
+            `)}`;
+          };
+
+          switch (label) {
+            case "typescript":
+            case "javascript":
+              return wrapForCrossDomain(`/min/vs/language/typescript/tsWorker.js`);
+
+            case "json":
+              return wrapForCrossDomain(`/min/vs/language/json/jsonWorker.js`);
+
+            case "html":
+              return wrapForCrossDomain(`/min/vs/language/html/htmlWorker.js`);
+
+            case "css":
+              return wrapForCrossDomain(`/min/vs/language/css/cssWorker.js`);
+
+            default:
+              return wrapForCrossDomain(`/min/vs/base/worker/workerMain.js`);
+          }
+        },
+      };
+    }
+  }, []);
 
   return (
     <ThemeContext.Consumer>
