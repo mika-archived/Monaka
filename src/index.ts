76b444f62d0213a58cc6f93711dff0eb7d6f76d3
Mikazuki <mikazuki_fuyuno@outlook.com>
2020-03-29 22:52:52 +0900

    feat: export components

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { default as IconProvider, IconContext } from "@/components/IconProvider";
+export { default as LanguageProvider, LanguageContext } from "@/components/LanguageProvider";
+export { default as ThemeProvider, ThemeContext } from "@/components/ThemeProvider";
+export { default as Monaka } from "@/components/Monaka";
