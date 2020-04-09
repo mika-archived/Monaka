53d0ea9c8ae6f297b3c42f753c598bf9299f9b93
Mikazuki <mikazuki_fuyuno@outlook.com>
2020-04-09 20:46:16 +0900

    fix: export more types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
+export { default as Input } from "@/components/Input";
 export { default as IconProvider, IconContext } from "@/components/IconProvider";
 export { default as LanguageProvider, LanguageContext } from "@/components/LanguageProvider";
+export { default as ProjectSection } from "@/components/Project/ProjectSection";
 export { default as ThemeProvider, ThemeContext } from "@/components/ThemeProvider";
 export { default as Monaka } from "@/components/Monaka";
+
+// export types
+export { FileIcon, FileItem, FileType, DirectoryItem, Item } from "@/types";
