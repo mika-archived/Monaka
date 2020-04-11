91f57e41f7ca6e3e0a925fad6f9e2b17970923a4
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2020-04-11 17:10:05 +0900

    feat: export Theme

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ export { default as Input } from "@/components/Input";
 export { default as IconProvider, IconContext } from "@/components/IconProvider";
 export { default as LanguageProvider, LanguageContext } from "@/components/LanguageProvider";
 export { default as ProjectSection } from "@/components/Project/ProjectSection";
-export { default as ThemeProvider, ThemeContext } from "@/components/ThemeProvider";
+export { default as ThemeProvider, ThemeContext, Theme } from "@/components/ThemeProvider";
 export { default as Monaka } from "@/components/Monaka";
 
 // export types
